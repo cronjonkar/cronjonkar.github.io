5f6a5ffd95005271c305db38cfaa4a1a1a5fd307
Onkar <onkar@cronj.com>
2019-09-24 18:05:55 +0530

    changes in response

--- app.js
+++ app.js
@@ -16,17 +16,19 @@ app.get('/getContacts', (req, res, next) => {
     const { organization_id } = req.query
     console.log(req.query)
     
-    if (!authorization) throw new Error(`You must send an Authorization header`)
+    if (!authorization) res.json(`You must send an Authorization header`)
 
-    if (!organization_id) throw new Error(`You must pass and organization id`)
+    if (!organization_id) res.json(`You must pass and organization id`)
 
     const [id] = organization_id.split(' ')
     const [token] = authorization.split(' ')
     thirdParty.getContacts(id, token).then(result => {
-        res.status(200).send({ msg: result.msg, contactList: result.data })
+        // res.status(200).send({ msg: result.msg, contactList: result.data })
+        res.json({ msg: result.msg, contactList: result.data })
     }
     ).catch(error => {
-        res.status(500).send({ msg: error, contactList: [] })
+        // res.status(500).send({ msg: error, contactList: [] })
+        res.json({ msg: error, contactList: [] })
     })
 
 
